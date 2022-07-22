9980e864d1d89b4d5c0bde36cd19bc6d3f765130
Amin <amin.bamyan79@gmail.com>
2022-07-22 17:05:51 +1200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Nauti Nav on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
